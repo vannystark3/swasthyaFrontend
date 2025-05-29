8ee3a23f768982a76f813c3f8be54548ad69f847
Aditya Khurana <151136783+vannystark3@users.noreply.github.com>
2025-05-29 06:36:31 +0530

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -12,7 +12,7 @@ const Login = ({ setLoggedIn }) => {
 
   const handleLogin = async () => {
   try {
-    const response = await fetch('http://localhost:5000/login', {
+    const response = await fetch('https://swasthyabackend.onrender.com/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ id, password, role })
